7eb94e6c736f0897347311738010beb849be9cda
DESKTOP-2F6PBUI\tbinh <tbinhduong0101@gmail.com>
2025-02-08 23:01:00 +0700

    Update BE code: API CRUD

---
+++ BE/user-service/src/main/java/com/swp/user_service/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.swp.user_service.controller;
+
+import com.swp.user_service.dto.request.UserCreationRequest;
+import com.swp.user_service.dto.request.UserUpdateRequest;
+import com.swp.user_service.entity.User;
+import com.swp.user_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted";
+    }
+}
---
+++ BE/user-service/src/main/java/com/swp/user_service/dto/request/UserUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.swp.user_service.dto.request;
+
+public class UserUpdateRequest {
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
