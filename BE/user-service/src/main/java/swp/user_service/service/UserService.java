221b1e5992e690b2a94ded26b9c7f3a9ae69b1c1
DESKTOP-2F6PBUI\tbinh <tbinhduong0101@gmail.com>
2025-02-09 14:23:31 +0700

    test fix

---
+++ BE/user-service/src/main/java/swp/user_service/service/UserService.java
@@ -0,0 +1,47 @@
+package swp.user_service.service;
+
+import swp.user_service.dto.request.UserCreationRequest;
+import swp.user_service.dto.request.UserUpdateRequest;
+import swp.user_service.entity.User;
+import swp.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request){
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
