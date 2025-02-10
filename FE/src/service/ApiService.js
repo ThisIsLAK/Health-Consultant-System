8d9ba193ccda261f43ef6507db6bf83ed71a002d
hoang <hoang390dbp@gmail.com>
2025-02-10 12:59:37 +0700

    add getHeader

--- FE/src/service/ApiService.js
+++ FE/src/service/ApiService.js
@@ -3,6 +3,14 @@ import axios from "axios";
 export default class ApiService {
     static BASE_URL = "http://localhost:8080";
 
+    static getHeader() {
+        const token = localStorage.getItem("token");
+        return {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+    }
+
     /**AUTh && USERS API */
     static async registerUser(registration) {
         const response = await axios.post(`${this.BASE_URL}/identity/users`, registration)
@@ -14,7 +22,6 @@ export default class ApiService {
         const response = await axios.post(`${this.BASE_URL}/identity/users/login`, loginDetails)
         return response.data;
     }
-
 
     static async getLoggedInUserInfo() {
         const response = await axios.get(`${this.BASE_URL}/user/my-info`, {
