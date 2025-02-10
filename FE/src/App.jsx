a27184b9b0056fe3ecd49b2d246da2e5e403b899
hoang <hoang390dbp@gmail.com>
2025-02-10 10:43:07 +0700

    fix route again

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,45 +1,26 @@
 import React from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import LoginSignup from './pages/login/LoginSignup';
 import Homepage from './pages/student/Homepage';
 import UserProfile from './pages/student/UserProfile';
 import ContactUs from './pages/student/ContactUs';
 import AboutUs from './pages/student/AboutUs';
+import LoginSignup from './pages/login/LoginSignup';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-    children: [
-      {
-        index: true,
-        element: <Homepage/>,
-      },
-      {
-        path: "/profile",
-        element: <UserProfile />,
-      },
-      {
-        path: "/contact",
-        element: <ContactUs />,
-      },
-      {
-        path: "/aboutus",
-        element: <AboutUs />,
-      },
-    ]
-  },
-  {
-    path: "/login",
-    element: <LoginSignup />,
-  },
-  
-]);
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path='/' element={<Homepage />} />
+        <Route path='/profile' element={<UserProfile />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path='/login' element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
