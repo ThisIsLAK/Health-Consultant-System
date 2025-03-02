ab8bfe067cd5ff2e0458fc685b2536f5704e77df
DESKTOP-2F6PBUI\tbinh <tbinhduong0101@gmail.com>
2025-03-02 18:37:28 +0700

    thêm swagger cho hoàn chỉnh

--- BE/user-service/src/main/java/com/swp/user_service/UserServiceApplication.java
+++ BE/user-service/src/main/java/com/swp/user_service/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package com.swp.user_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title ="Library APIS", version = "1.0", description = "Library Management APIs."))
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
