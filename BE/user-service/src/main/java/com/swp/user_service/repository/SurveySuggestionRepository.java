b19c6f5e7859c6b357a2b6c35b36f8900aa8a5b9
DESKTOP-2F6PBUI\tbinh <tbinhduong0101@gmail.com>
2025-03-19 22:24:13 +0700

    Xóa suggestion

--- BE/user-service/src/main/java/com/swp/user_service/repository/SurveySuggestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swp.user_service.repository;
-
-import com.swp.user_service.entity.SurveySuggestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveySuggestionRepository extends JpaRepository<SurveySuggestion, String> {
-}
-
