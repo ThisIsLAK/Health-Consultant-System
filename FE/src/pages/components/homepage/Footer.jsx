a97325b4ca8491746aec0ee0dec2b851713b3f43
hoang <hoang390dbp@gmail.com>
2025-04-01 16:41:24 +0700

    fix footer to match links

--- FE/src/pages/components/homepage/Footer.jsx
+++ FE/src/pages/components/homepage/Footer.jsx
@@ -2,50 +2,52 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { useNavigate } from "react-router-dom";
 
+const Footer = () => {
+    const navigate = useNavigate();
 
-const Footer = () => (
-    <footer className="footer-container">
-        <div className="footer-column">
-            <img src="/logo.png" alt="FPT High School" className="footer-logo" />
-            <p>Address: FPT University, District 9, HCMC</p>
-            <p>Email: duongtbse182174@fpt.edu.vn</p>
-            <p>Phone: (+84) 917917669</p>
-            <p>&copy; Copyright 2024</p>
-            <div className="footer-social">
-                <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
-                <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
-                <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
-            </div>
-        </div>
-        <div className="policies-footer-column">
+    return (
+        <footer className="footer-container">
             <div className="footer-column">
-                <h4>My Account</h4>
-                <ul>
-                    <li><a href="#">Sign In</a></li>
-                    <li><a href="#">Dashboard</a></li>
-                    <li><a href="#">Monitor Progress</a></li>
-                    <li><a href="#">Compare Success</a></li>
-                    <li><a href="#">My Topics</a></li>
-                </ul>
+                <div className="navbar-brand font-bold text-xl text-white mb-3" onClick={() => navigate("/")}>FPT Support</div>
+                <p>Address: FPT University, District 9, HCMC</p>
+                <p>Email: duongtbse182174@fpt.edu.vn</p>
+                <p>Phone: (+84) 917917669</p>
+                <p>&copy; Copyright 2024</p>
+                <div className="footer-social">
+                    <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
+                    <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+                    <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+                </div>
             </div>
-            <div className="footer-column">
-                <h4>About SBA</h4>
-                <ul>
-                    <li><a href="#">Company Information</a></li>
-                    <li><a href="#">Resources</a></li>
-                    <li><a href="#">Our Success</a></li>
-                    <li><a href="#">Meet The Experts</a></li>
-                </ul>
+            <div className="policies-footer-column">
+                <div className="footer-column">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><a href="/tests">Multiple Test</a></li>
+                        <li><a href="/support">Support Program</a></li>                       
+                        <li><a href="/booking">Booking Psychologist</a></li>                       
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>About FPT Support</h4>
+                    <ul>
+                        <li><a href="/aboutus">About Us</a></li>
+                        <li><a href="/blog">Blogs</a></li>
+                        <li><a href="#">Our Success</a></li>
+                        <li><a href="#">Meet The Experts</a></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Support</h4>
+                    <ul>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
             </div>
-            <div className="footer-column">
-                <h4>Support</h4>
-                <ul>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
-);
+        </footer>
+    );
+};
 
 export default Footer;
